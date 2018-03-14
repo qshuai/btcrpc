449680af4a4994fe50493f034a234ba79a2f6212
qshuai <qishuai231@gmail.com>
2018-03-14 10:53:53 +0800

    log and configuration deploy

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"encoding/hex"
-	"fmt"
 
+	"github.com/astaxie/beego/config"
+	"github.com/astaxie/beego/logs"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/wire"
 )
 
+// global variable for log
+var log = logs.NewLogger()
+
 func main() {
+	// log setting
+	logs.SetLogger(logs.AdapterFile, `{"filename":"log/btcrpc.log"}`)
+
+	// configuration setting
+	conf, _ := config.NewConfig("ini", "conf/app.conf")
+
+	// acquire configure item
+	link := conf.String("rpc:url") + conf.String("rpc:url")
+	user := conf.String("rpc:user")
+	passwd := conf.String("rpc:passwd")
+
+	// rpc client instance
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "localhost:18332",
-		User:         "rpc",
-		Pass:         "g6eJ6ZLCiws7mc0SVwlWhI7h4ve5yH",
+		Host:         link,
+		User:         user,
+		Pass:         passwd,
 		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
 		DisableTLS:   true, // Bitcoin core does not provide TLS by default
 	}
@@ -25,7 +41,7 @@ func main() {
 	}
 	defer client.Shutdown()
 
-	// get listunspent
+	// rpc requestss to get unspent coin list
 	lu, err := client.ListUnspent()
 	if err != nil {
 		panic(err)
@@ -35,9 +51,12 @@ func main() {
 	msg.TxIn = make([]*wire.TxIn, 1)
 	msg.TxOut = make([]*wire.TxOut, 1)
 
+	// only support P2PKH transaction
 	for i := 0; i < len(lu); i++ {
+		// construct a P2PKH transaction
 		msg.LockTime = 0
 		hash, _ := chainhash.NewHashFromStr(lu[i].TxID)
+		// txin
 		txin := wire.TxIn{
 			PreviousOutPoint: wire.OutPoint{
 				Hash:  *hash,
@@ -46,23 +65,35 @@ func main() {
 			Sequence: 0xffffff,
 		}
 
+		// txout
 		var pkScript []byte
 		if i >= len(lu)-1 {
 			pkScript, _ = hex.DecodeString(lu[0].ScriptPubKey)
 		} else {
 			pkScript, _ = hex.DecodeString(lu[i+1].ScriptPubKey)
 		}
-		txout := wire.TxOut{
+		out := wire.TxOut{
 			Value:    int64(lu[i].Amount * 1e9 * 0.9),
 			PkScript: pkScript,
 		}
 
 		msg.TxIn[0] = &txin
-		msg.TxOut[0] = &txout
+		msg.TxOut[0] = &out
+
+		// rpc requests signing a raw transaction and gets returned signed transaction,
+		// or get null and a err reason
+		signed, _, err := client.SignRawTransaction(msg)
+		if err != nil {
+			log.Error(err.Error())
+		}
+
+		// rpc request send a signed transaction, it will return a error if there are any
+		// error
+		ret := client.SendRawTransactionAsync(signed, true)
+		if txhash, err := ret.Receive(); err != nil {
+			log.Error(err.Error())
+		} else {
+			log.Info("Create a transaction success, txhash: %s", txhash.String())
+		}
 	}
-
-	signtx, _, _ := client.SignRawTransaction(msg)
-
-	ret := client.SendRawTransactionAsync(signtx, true)
-	fmt.Println(ret.Receive())
 }
