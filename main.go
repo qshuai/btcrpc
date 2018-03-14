356abcd3aa7f0cff2861804c9661703195e3c4af
qshuai <qishuai231@gmail.com>
2018-03-14 20:48:46 +0800

    create a lot of transaction

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"encoding/hex"
-
 	"github.com/astaxie/beego/config"
 	"github.com/astaxie/beego/logs"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/btcsuite/btcd/txscript"
 )
 
 // global variable for log
 var log = logs.NewLogger()
+
+// store avaiable input and output
+var input = make(map[chainhash.Hash]float64)
+var output = make(map[string][]byte)
 
 func main() {
 	// log setting
@@ -21,9 +25,9 @@ func main() {
 	conf, _ := config.NewConfig("ini", "conf/app.conf")
 
 	// acquire configure item
-	link := conf.String("rpc:url") + conf.String("rpc:url")
-	user := conf.String("rpc:user")
-	passwd := conf.String("rpc:passwd")
+	link := conf.String("rpc::url") + ":" + conf.String("rpc::port")
+	user := conf.String("rpc::user")
+	passwd := conf.String("rpc::passwd")
 
 	// rpc client instance
 	connCfg := &rpcclient.ConnConfig{
@@ -41,44 +45,40 @@ func main() {
 	}
 	defer client.Shutdown()
 
-	// rpc requestss to get unspent coin list
-	lu, err := client.ListUnspent()
-	if err != nil {
-		panic(err)
-	}
+	rangeAccount(client)
+	inputs(client)
 
 	msg := wire.NewMsgTx(1)
 	msg.TxIn = make([]*wire.TxIn, 1)
 	msg.TxOut = make([]*wire.TxOut, 1)
 
 	// only support P2PKH transaction
-	for i := 0; i < len(lu); i++ {
+	for hash, amount := range input {
 		// skip if the balance of this bitcoin address is zero
-		if lu[i].Amount < 1e-5 {
+		if amount < 1e-5 {
 			continue
 		}
 
 		// construct a P2PKH transaction
 		msg.LockTime = 0
-		hash, _ := chainhash.NewHashFromStr(lu[i].TxID)
+
 		// txin
 		txin := wire.TxIn{
 			PreviousOutPoint: wire.OutPoint{
-				Hash:  *hash,
+				Hash:  hash,
 				Index: 0,
 			},
 			Sequence: 0xffffff,
 		}
 
 		// txout
-		var pkScript []byte
-		if i >= len(lu)-1 {
-			pkScript, _ = hex.DecodeString(lu[0].ScriptPubKey)
-		} else {
-			pkScript, _ = hex.DecodeString(lu[i+1].ScriptPubKey)
+		pkScript := getRandScriptPubKey()
+		if pkScript == nil {
+			panic("no account in output...")
 		}
+
 		out := wire.TxOut{
-			Value:    int64(lu[i].Amount * 1e9 * 0.9),
+			Value:    int64(amount * 1e8 * 0.9),
 			PkScript: pkScript,
 		}
 
@@ -100,5 +100,49 @@ func main() {
 		} else {
 			log.Info("Create a transaction success, txhash: %s", txhash.String())
 		}
+	}
+}
+
+func rangeAccount(client *rpcclient.Client) {
+	addresses, err := client.GetAddressesByAccount("")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range addresses {
+		ret, _, err := base58.CheckDecode(item.String())
+		if err != nil {
+			panic(err)
+		}
+
+		final, err := txscript.NewScriptBuilder().AddOp(txscript.OP_DUP).AddOp(txscript.OP_HASH160).
+			AddData(ret).AddOp(txscript.OP_EQUALVERIFY).AddOp(txscript.OP_CHECKSIG).
+			Script()
+
+		if err != nil {
+			panic(err)
+		}
+		output[item.String()] = final
+	}
+}
+
+// map return random item
+func getRandScriptPubKey() []byte {
+	for _, item := range output {
+		return item
+	}
+	return nil
+}
+
+func inputs(client *rpcclient.Client) {
+	// rpc requestss to get unspent coin list
+	lu, err := client.ListUnspent()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range lu {
+		hash, _ := chainhash.NewHashFromStr(item.TxID)
+		input[*hash] = item.Amount
 	}
 }
